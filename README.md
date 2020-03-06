c158bdb63dc46ae2cd12bb6a5e3debd69a1b3c39
ceciliaccwei <ceciliawei@yahoo.com>
2020-03-06 12:22:14 -0700

    added reference

--- README.md
+++ README.md
@@ -14,8 +14,10 @@ Generally everything is LICENSE'D under the Apache 2 license by Abram Hindle.
 
 References
 ========================
+[Dr.Hindle's Ajax Slides](https://github.com/abramhindle/CMPUT404-AJAX-Slides/blob/master/ObserverExample/server.py)
+
 [XMLHttpRequest Send Request](https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/send)
 
 [Pass empty function as parameter](https://stackoverflow.com/questions/17685745/empty-functions-in-javascript)
 
-[Dr.Hindle's Ajax Slides](https://github.com/abramhindle/CMPUT404-AJAX-Slides/blob/master/ObserverExample/server.py)
+[Difference between flask.jsonify and json.dump](https://stackoverflow.com/questions/7907596/json-dumps-vs-flask-jsonify)
--- server.py
+++ server.py
@@ -22,7 +22,7 @@
 
 
 import flask
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, jsonify
 import json
 app = Flask(__name__)
 app.debug = True
@@ -82,23 +82,24 @@ def update(entity):
     data = flask_post_json()
     for key, value in data.items():
         myWorld.update(entity, key, value)
+    # flask has a security restriction in jsonify
     return json.dumps(myWorld.get(entity))
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return json.dumps(myWorld.world())
+    return flask.jsonify(myWorld.world())
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return json.dumps(myWorld.get(entity))
+    return flask.jsonify(myWorld.get(entity))
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
     myWorld.clear()
-    return json.dumps(myWorld.world())
+    return flask.jsonify(myWorld.world())
 
 if __name__ == "__main__":
     app.run()
